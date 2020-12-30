4e5fd20a5db8d6d5716a571a9d3d692e54ade9d1
Teles <jose.maciel@vivadecora.com.br>
2020-12-30 10:47:32 -0300

    #3 - Mantem urls absolutas nas imagens

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const addLazyLoadingToImageTag = content => content.replace(/<img\b/gi, '<img loading=\'lazy\'')
+const prependSiteUrlToImageSrc = (content, config) => {
+    return content.replace(/<img\b([^>]+)src=(["'])([^'"]+)(["'])/gi, `<img $1src=$2${config.url}$3$4`)
+};
+
+hexo.extend.filter.register('after_post_render', function(data) {
+    const { config } = this;
+    data.content = addLazyLoadingToImageTag(data.content);
+    data.content = prependSiteUrlToImageSrc(data.content, config);
+    return data;
+});
